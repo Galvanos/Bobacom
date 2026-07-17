d43ae1b0f37a193d1b525a7e4c14b02ede32d434
Iresha Warnakulasuriya <127923225+iresha-warnakulasuriya@users.noreply.github.com>
2026-07-17 12:08:26 +0200

    added the crud methods to ordine controller

--- src/main/java/com/bobacom/backend/controller/OrdineController.java
+++ src/main/java/com/bobacom/backend/controller/OrdineController.java
@@ -1,9 +1,17 @@
 package com.bobacom.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
- 
+import com.bobacom.backend.dto.input.IngredienteRequest;
+import com.bobacom.backend.dto.input.OrdineRequest;
+import com.bobacom.backend.dto.output.ResponseDTO;
 import com.bobacom.backend.service.interfaces.IOrdineService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +23,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/rest/ordine")
 public class OrdineController {
 //	private Carrello carrello;
-	private IOrdineService orderS;
+	private IOrdineService orderService;
+	
+	@PostMapping("create")
+	public ResponseEntity<ResponseDTO> create(@RequestBody (required=true) OrdineRequest request) throws Exception{
+		orderService.create(request);
+		return ResponseEntity.ok(ResponseDTO.builder().msg("Ingrediente aggiunto").build());
+	}
+	
+	@PatchMapping("update")
+	public ResponseEntity<ResponseDTO> update(@RequestBody (required=true) OrdineRequest request) throws Exception{
+		orderService.update(request);
+		return ResponseEntity.ok(ResponseDTO.builder().msg("Ingrediente modificato").build());
+	}
+	
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<ResponseDTO> delete(@PathVariable (required=true) Integer id) throws Exception{
+		orderService.delete(id);
+		return ResponseEntity.ok(ResponseDTO.builder().msg("Ingrediente eliminato").build());
+	}
 	
 }
