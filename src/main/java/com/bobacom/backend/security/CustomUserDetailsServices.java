33f68f7ab4e3a58049f5ddbedaa5179837317d18
Marco Scarpa <marco.scarpa@tiscali.it>
2026-07-20 16:30:52 +0200

    risolto problema di dipendenza circolare tra UserService e CustomUserDetailsServices

--- src/main/java/com/bobacom/backend/security/CustomUserDetailsServices.java
+++ src/main/java/com/bobacom/backend/security/CustomUserDetailsServices.java
@@ -11,6 +11,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import com.bobacom.backend.dto.output.UtenteDTO;
+import com.bobacom.backend.model.Utente;
+import com.bobacom.backend.repository.IUtenteRepository;
 import com.bobacom.backend.service.interfaces.IUtenteService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,16 +23,16 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class CustomUserDetailsServices {
 
-	private final IUtenteService utenteService;
+	private final IUtenteRepository utenteRepository;//usato repository perché se si usasse il service per la list si creerebbe una dipendenza circolare per aggiornare l'utente in memory
 	private final PasswordEncoder getPasswordEncoder;
 	
 	
 	public InMemoryUserDetailsManager loadUser() throws Exception {
 		log.debug("loadUser ....");
 		
-		List<UtenteDTO> list;
+		List<Utente> list;
 		try {
-			list = utenteService.list();
+			list = utenteRepository.findAll();
 		} catch (Exception e) {
 			//si può solo loggare un errore perché si tratta di cosa assai grave non rimediabile
 			log.error(e.getMessage());
