8a4fa3dd6b385f0d567f62f4b5d32833e2c64278
Luca Ongaro <48388970+Galvanos@users.noreply.github.com>
2026-07-21 12:16:55 +0200

    add tests for ingrediente

--- src/test/java/com/bobacom/backend/service/TestIngredienteService.java
+++ src/test/java/com/bobacom/backend/service/TestIngredienteService.java
@@ -1,12 +1,17 @@
 package com.bobacom.backend.service;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.jdbc.Expectations;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -31,7 +36,6 @@ import tools.jackson.databind.ObjectMapper;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 public class TestIngredienteService {
 	@Autowired
 	private MockMvc mockMvc;
@@ -67,6 +71,58 @@ public class TestIngredienteService {
 					).andExpect(status().isOk());
 		} catch (Exception e) {
 			log.error("Error nel creare ingrediente: {}", e.getMessage());
+		}
+	}
+	
+	@Order(2)
+	@Test
+	void updateTest() throws Exception{
+		CategoriaIngrediente cat = CategoriaIngrediente.builder().nome("sfizioseria").build();
+		cat = catRepo.save(cat);
+		Allergeni all = Allergeni.builder().nome("Blumele").build();
+		all = allRepo.save(all);
+		List<Integer> allList = new ArrayList<Integer>();
+		allList.add(all.getId());
+		IngredienteRequest request = IngredienteRequest.builder()	.id(1)
+																	.nome("Appine")
+																	.descrizione("Mappenze")
+																	.colore("YELLOW")
+																	.prezzoRestock(1f)
+																	.quantitaStock(BigDecimal.valueOf(30))
+																	.sovraprezzoAggiunta(0.7f)
+																	.idCategoriaIngrediente(cat.getId())
+																	.idAllergene(allList)
+																	.build();
+		try {
+			mockMvc.perform(patch("/rest/ingrediente/update")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))
+			).andExpect(status().isOk());
+		} catch (Exception e) {
+			log.error("Error nell' update di un ingrediente: {}", e.getMessage());
+		}	
+	}
+	
+	@Order(3)
+	@Test
+	void deleteTest() throws Exception{
+		try {
+			mockMvc.perform(delete("/rest/ingrediente/delete/1")
+			.contentType(MediaType.APPLICATION_JSON)
+			).andExpect(status().isOk());
+		} catch (Exception e) {
+			log.error("Error nell' update di un ingrediente: {}", e.getMessage());
+		}	
+	}
+	@Order(4)
+	@Test
+	void listTest() throws Exception{
+		try {
+			mockMvc.perform(get("/rest/ingrediente/list")
+					.contentType(MediaType.APPLICATION_JSON)
+					).andExpect(status().isOk()).andExpect(jsonPath("$").isArray());
+		} catch (Exception e) {
+			log.error("Error nell' update di un ingrediente: {}", e.getMessage());
 		}
 	}
 }
