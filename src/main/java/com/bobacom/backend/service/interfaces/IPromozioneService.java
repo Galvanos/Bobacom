317c4017d445a99ebb429a83a9e66fcb1b3a1871
AlbertoPvtt <59047885+AlbertoPvtt@users.noreply.github.com>
2026-07-21 10:45:21 +0200

    Promozione Service

--- src/main/java/com/bobacom/backend/service/interfaces/IPromozioneService.java
+++ src/main/java/com/bobacom/backend/service/interfaces/IPromozioneService.java
@@ -1,5 +1,15 @@
 package com.bobacom.backend.service.interfaces;
 
-public interface IPromozioneService {
+import java.util.List;
 
+import com.bobacom.backend.dto.input.PromozioneRequest;
+import com.bobacom.backend.dto.output.PromozioneDTO;
+
+public interface IPromozioneService {
+	void create(PromozioneRequest req) throws Exception;
+	void update(PromozioneRequest req);
+	void delete(Integer id);
+	
+	List<PromozioneDTO> list() throws Exception;
+	PromozioneDTO getById(Integer id) throws Exception;
 }
